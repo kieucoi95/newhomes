491f397a3011d5b3999823b18e662f0dc42f38bf
DESKTOP-UBERTA9\Sea Chan <thanhhaitran1404@gmail.com>
2021-09-12 16:08:16 +0700

    Footer / Header updated

--- web/themes/newhomes/js/ht-script.js
+++ web/themes/newhomes/js/ht-script.js
@@ -15,6 +15,40 @@
     };
 
     Drupal.ht_main.header = function() {
-        console.log(2);
+        // Search box
+        $('.search-btn>a').click(function() {
+            if ($(this).hasClass('icon-search')) {
+                $(this).hide();
+                $('.search-btn>a.icon-close').show();
+                $(this).parents('._header').find('.sb').show();
+            } else {
+                $(this).hide();
+                $('.search-btn>a.icon-search').show();
+                $(this).parents('._header').find('.sb').hide();
+            }
+        });
+
+        // Lang switcher
+        $('.lang-nav a').click(function() {
+            let langcode = $(this).attr('lang');
+            window.location.href = jQuery('a[hreflang="' + langcode + '"]').attr('href');
+        });
+
+        // Sticky header
+        $(window).scroll(function() {
+            if ($(document).scrollTop() >= 100) {
+                $('header').addClass('header-sticky');
+            } else {
+                $('header').removeClass('header-sticky');
+            }
+        });
+
+        // Mobile header
+        $('.mb-btn').click(function() {
+            $('.mb-header').toggleClass('open');
+            $('.search-btn>a.icon-close').hide();
+            $('.search-btn>a.icon-search').show();
+            $(this).parents('._header').find('.sb').hide();
+        });
     };
 })(jQuery, Drupal);
